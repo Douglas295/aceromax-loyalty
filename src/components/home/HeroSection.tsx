04ab9d06c3dcc24ffbeb1eab8b066ff2e3d3e271
DevKage <suzukiken174@gmail.com>
2025-09-26 02:39:46 -0500

    fix homepage image mobile mode

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -1,36 +1,18 @@
-import React from "react"; 
+import React from "react";
+import Image from "next/image";
+
 export default function HeroSection() {
   return (
-    <div className="aspect-[2/1] bg-[url(https://aceromax.mx/wp-content/uploads/2024/07/Distribuidores-exclusivos-7.svg)] bg-cover bg-no-repeat p-[14%]">
-      {/*
-      <div className="flex flex-col">
-      <h1
-        className="text-2xl md:text-4xl lg:text-6xl font-[400] text-[#555555] font-montserrat transition-opacity duration-1000 ease-in-out ml-4"
-      >
-        <span className="block font-[200]">
-          Purchase
-        </span>
-        <span className="text-[#8A782D]"> Many Items </span>
-        <div>
-          <span className="font-[200] text-[#555555]">
-            for the
-          </span>
-        </div>
-        <div>
-          <span className="text-[#8A782D]">Future</span>
-        </div>
-      </h1>
-
-      <div className="mt-2 sm:mt-6">
-        <a
-          href="/dashboard"
-          className="inline-block hover:font-[700] btn-intro text-white text-[12px] sm:text-[16px] px-4 py-2 sm:px-6 sm:py-3 shadow hover:opacity-90 transition rounded-full"
-        >
-          Finance Analysis &gt;
-        </a>
+    <section className="w-full">
+      <div className="relative w-full min-h-[500px] sm:min-h-[600px] md:min-h-[700px] lg:min-h-[800px] xl:min-h-[900px]">
+        <Image
+          src="https://aceromax.mx/wp-content/uploads/2024/07/Distribuidores-exclusivos-7.svg"
+          alt="Hero Banner"
+          layout="fill"
+          objectFit="cover"
+          priority
+        />
       </div>
-      </div>
-      */}
-    </div>
+    </section>
   );
 }
